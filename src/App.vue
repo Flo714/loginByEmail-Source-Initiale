1eeec154effabe368f76024d7937f5bdfc3f2138
Florentin Demortiere <florentin.demortiere@edu.univ-fcomte.fr>
2022-09-23 17:00:28 +0200

    auth Google

--- src/App.vue
+++ src/App.vue
@@ -5,27 +5,13 @@ import { SupabaseAuthClient } from '@supabase/supabase-js/dist/module/lib/Supaba
 </script>
 
 <template>    
-  <header>
-    <router-link to="/">Go to Home</router-link>
-    <img alt="Logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-    <div class="wrapper" id="signOut">
-      <div><SignIn msg="User, please sign in !" /></div>
-      <label>email: </label><br>
-	    <input type="email" required v-model="email" placeholder="username@domain.tld"><br>
-	    <label>password: </label><br>
-	    <input type="password" required v-model="passwd" ><br>
-      <button v-on:click="register()">Sign Up</button>
-      <button v-on:click="login()">Sign In</button>
-      <button v-on:click="reset()">Reset</button>
-      <p>
-      <label id="status"> You are not yet connected </label><br>  
-      </p>
-    </div>
-  </header>
-  
-  <main>
-    
-  </main>
+  <h1>{{ msg }}</h1> 
+    <p> 
+      Please login if you have an account or register : 
+    </p> 
+    <button @click="login()">Sign In</button><br> 
+    <button @click="logout()">Sign Out</button><br> 
+    <label id="status">You are not yet logged !  </label> 
 </template>
 
 <script>
@@ -34,54 +20,39 @@ const SUPABASE_URL = 'https://bzciqcbnmsgtsuflbcjy.supabase.co'
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ6Y2lxY2JubXNndHN1ZmxiY2p5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjMzMzcyNDEsImV4cCI6MTk3ODkxMzI0MX0.b52EyXMgTnXNPGjWmEKp5z34Ezx31LVHgbDLTh8kvb8'
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
+supabase.auth.onAuthStateChange((event, session) => { 
+  if(session==null){ 
+    document.getElementById('status').innerHTML='You are not logged !!!'; 
+  } else{ 
+    //alert('session value: ' + JSON.stringify(session)) 
+    document.getElementById('status').innerHTML='You are logged with the email: ' + session.user.email; 
+  } 
+})
 
 export default {
   methods: {  
-    //this method allows a new user to sign up the system. Once done, the user receives an email
-    //asking for account validation. Once the validation made the user is added to the system
-    async register(){ 
+    //this method allows to release the connexion with the Google account 
+    async logout(){ 
       try { 
-        const { user, session, error } = await supabase.auth.signUp({ 
-          email: this.email, 
-          password: this.passwd, 
-        }); 
+        const { user, session, error } = await supabase.auth.signOut(); 
         if (error) throw error; 
-        document.getElementById('status').innerHTML='Please validate the received email !' 
+        document.getElementById('status').innerHTML='You are disconnected !' 
       } catch (error) { 
         alert(error.error_description || error.message); 
       }  
     }, 
-    
-    //this method allows the already registred user to log in the system.
-    async login(){
+    //this method allows to log in the system using Google provider 
+     
+  async login(){ 
       try { 
         const { user, session, error } = await supabase.auth.signIn({ 
-          email: this.email, 
-          password: this.passwd, 
-        });
+          provider: 'google', 
+        }); 
         if (error) throw error; 
-        document.getElementById('status').innerHTML='You are now logged !' 
       } catch (error) { 
         alert(error.error_description || error.message); 
       }  
-    },
-    async reset() {
-      const { data, error } = await supabase.auth.api.resetPasswordForEmail(
-        this.email
-      )
-    },
-  },
-  mounted() {
-    supabase.auth.onAuthStateChange(async (event, session) => {
-      if (event == 'PASSWORD_RECOVERY') {
-        const newPassword = prompt('What would you like your new password to be?')
-        const { data, error } = await supabase.auth.update({
-          password: newPassword,
-        })
-        if (data) alert('Password updated successfully!')
-        if (error) alert('There was an error updating your password.')
-      }
-    })
+    }
   }
 }
 </script>
